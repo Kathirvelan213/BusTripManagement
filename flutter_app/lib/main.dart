e934ac7573f0bb08f5fc0db5e66f39da75fb82ec
kathirvelan <kathirvelanj1@gmail.com>
2025-09-07 17:59:23 +0530

    Resolved remaining merge markers in pubspec and AndroidManifest

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:flutter_app/components/pages/tracking/tracking_page.dart';
 import 'package:flutter_app/services/hub_services/location_hub_service.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -8,10 +7,6 @@ void main() async {
   await dotenv.load(fileName: ".env.production");
   final LocationHubService locationHubService = LocationHubService();
   await locationHubService.start();
-=======
-
-void main() {
->>>>>>> d00d9210a4ba15460a5cb47e90bea4d55e709dd8
   runApp(const MainApp());
 }
 
@@ -23,11 +18,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-<<<<<<< HEAD
           child: TrackingPage(),
-=======
-          child: Text('Hello World!'),
->>>>>>> d00d9210a4ba15460a5cb47e90bea4d55e709dd8
         ),
       ),
     );
