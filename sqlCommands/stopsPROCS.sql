4825e32790d97d3474f3ff53015eada2fefcc077
kathirvelan <kathirvelanj1@gmail.com>
2025-09-09 23:00:11 +0530

    added sql procs for table creation and crud for routes,stops,route_stops tables

---
+++ sqlCommands/stopsPROCS.sql
@@ -0,0 +1,55 @@
+-- CREATE
+CREATE PROCEDURE sp_CreateStop
+    @name NVARCHAR(100),
+    @lat DECIMAL(9,6),
+    @lng DECIMAL(9,6)
+AS
+BEGIN
+    INSERT INTO stops (name, lat, lng)
+    VALUES (@name, @lat, @lng);
+
+    SELECT SCOPE_IDENTITY() AS NewStopId;
+END
+GO
+
+-- READ (all)
+CREATE PROCEDURE sp_GetStops
+AS
+BEGIN
+    SELECT * FROM stops;
+END
+GO
+
+-- READ (by ID)
+CREATE PROCEDURE sp_GetStopById
+    @stop_id INT
+AS
+BEGIN
+    SELECT * FROM stops WHERE stop_id = @stop_id;
+END
+GO
+
+-- UPDATE
+CREATE PROCEDURE sp_UpdateStop
+    @stop_id INT,
+    @name NVARCHAR(100),
+    @lat DECIMAL(9,6),
+    @lng DECIMAL(9,6)
+AS
+BEGIN
+    UPDATE stops
+    SET name = @name,
+        lat = @lat,
+        lng = @lng
+    WHERE stop_id = @stop_id;
+END
+GO
+
+-- DELETE
+CREATE PROCEDURE sp_DeleteStop
+    @stop_id INT
+AS
+BEGIN
+    DELETE FROM stops WHERE stop_id = @stop_id;
+END
+GO
