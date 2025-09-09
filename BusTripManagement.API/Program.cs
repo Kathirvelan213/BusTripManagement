8c308c176e430b518b58f107bd4cff3f7330e35c
kathirvelan <kathirvelanj1@gmail.com>
2025-09-09 23:03:19 +0530

    removed merge markers from backend

--- BusTripManagement.API/Program.cs
+++ BusTripManagement.API/Program.cs
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
-
 using BusTripManagement.API.Hubs;
-
-=======
->>>>>>> d00d9210a4ba15460a5cb47e90bea4d55e709dd8
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-<<<<<<< HEAD
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
@@ -22,9 +16,6 @@ builder.Services.AddCors(options =>
 
 builder.Services.AddControllers();
 builder.Services.AddSignalR();
-=======
-builder.Services.AddControllers();
->>>>>>> d00d9210a4ba15460a5cb47e90bea4d55e709dd8
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -32,11 +23,7 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-<<<<<<< HEAD
-//if (app.Environment.IsDevelopment())
-=======
 if (app.Environment.IsDevelopment())
->>>>>>> d00d9210a4ba15460a5cb47e90bea4d55e709dd8
 {
     app.UseSwagger();
     app.UseSwaggerUI();
@@ -44,18 +31,11 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-<<<<<<< HEAD
 app.UseCors("AllowFrontend");
-
 
 app.UseAuthorization();
 
 app.MapControllers();
 app.MapHub<LiveLocationHub>("/location-hub");
-=======
-app.UseAuthorization();
-
-app.MapControllers();
->>>>>>> d00d9210a4ba15460a5cb47e90bea4d55e709dd8
 
 app.Run();
